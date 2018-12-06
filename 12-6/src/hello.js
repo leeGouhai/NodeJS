361fa271fddd256e26635b0687ec6f24aa59a3ae
leeG <576729174@qq.com>
2018-12-06 22:32:33 +0800

    做个测试

--- 12-6/src/hello.js
+++ 12-6/src/hello.js
@@ -1,9 +1,9 @@
 //这里的JS不能在浏览器端运行
 //只能在node环境(服务器端)下执行
 
-// let person = require("./js/person.js");
+let person = require("./js/person.js");
 
 //对象解构
 let {setName, setAge} = require("./js/person.js");
 
-console.log(setName(),setAge());
+console.log(person,setName(),setAge());
